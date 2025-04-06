988a54bc43f87dbbb6a74adde14815456db5c0b1
Aly Ashour <alyashour1@gmail.com>
2025-04-06 17:36:40 -0400

    rename ledger

--- Ledger/src/routes/account/+page.svelte
+++
@@ -1,7 +0,0 @@
-<!-- src/routes/account/+page.svelte -->
-<script lang="ts">
-    // You can add account-specific logic here later.
-</script>
-
-<h1>Account</h1>
-<p>This is your account page. More features coming soon.</p>
